07cdc2ac0eb50a0ec1b607856fd81e1c3855459c
Ryan Ashby <rjapsu160@gmail.com>
2022-02-11 10:25:41 -0800

    create basic html layout for currency input, and dropdown input

--- src/currency.js
+++ src/currency.js
@@ -1,11 +1,11 @@
 export default class CurrencyService {
-  static getExchange() {
+  static async getExchange() {
     try {
       const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
-        if (!response.ok) {
-          throw Error(response.error-type);
-        }
-        return response.json(); 
+      if (!response.ok) {
+        throw Error(response.error-type);
+      }
+      return response.json(); 
     } catch(error) {
       return error.message;
     }
