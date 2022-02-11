bddb58e7fa8a55b9b46f35a7ebc5c69da5e68fab
Ryan Ashby <rjapsu160@gmail.com>
2022-02-11 11:40:06 -0800

    change results to give toFixed(2) decimals

--- src/index.js
+++ src/index.js
@@ -5,37 +5,37 @@ import './css/styles.css';
 import CurrencyService from './currency';
 
 function getMoney(response) {
-  let amount = parseInt($("#amount").val());
+  let amount = parseInt($("#amount").val()).toFixed(2);
   let foreign = $("select option:selected").val();
   let country = $("select option:selected").text();
   if (response.conversion_rates) {
     if (foreign === "AZN") {
       let converted = `${response.conversion_rates.AZN}`;
-      let final = amount * converted;
+      let final = (amount * converted).toFixed(2);
       $('.output').text(`Your ${amount} US Dollars are worth ${final} ${country}`);
     } else if (foreign === "CDF") {
       let converted = `${response.conversion_rates.CDF}`;
-      let final = amount * converted;
+      let final = (amount * converted).toFixed(2);
       $('.output').text(`Your ${amount} US Dollars are worth ${final} ${country}`);
     } else if (foreign === "FJD") {
       let converted = `${response.conversion_rates.FJD}`;
-      let final = amount * converted;
+      let final = (amount * converted).toFixed(2);
       $('.output').text(`Your ${amount} US Dollars are worth ${final} ${country}`);
     } else if (foreign === "HRK") {
       let converted = `${response.conversion_rates.HRK}`;
-      let final = amount * converted;
+      let final = (amount * converted).toFixed(2);
       $('.output').text(`Your ${amount} US Dollars are worth ${final} ${country}`);
     } else if (foreign === "MVR") {
       let converted = `${response.conversion_rates.MVR}`;
-      let final = amount * converted;
+      let final = (amount * converted).toFixed(2);
       $('.output').text(`Your ${amount} US Dollars are worth ${final} ${country}`);
     } else if (foreign === "TRY") {
       let converted = `${response.conversion_rates.TRY}`;
-      let final = amount * converted;
+      let final = (amount * converted).toFixed(2);
       $('.output').text(`Your ${amount} US Dollars are worth ${final} ${country}`);
     }
   } else {
-    $('.showErrors').text(`There was an error: ${response.result}`);
+    $('.showErrors').text(`There was an error processing your request: ${response.result}`);
   }
 }
 
