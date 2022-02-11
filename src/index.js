9d21215deff0bc52a334e6296c9028f12bbe1346
Ryan Ashby <rjapsu160@gmail.com>
2022-02-11 09:42:28 -0800

    write basic doc.ready, call, and data grab funcs for index.js

--- src/index.js
+++ src/index.js
@@ -3,3 +3,23 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyService from './currency';
+
+function getMoney(response) {
+  if (response.conversion_rates) {
+   
+  } else {
+    $('.showErrors').text(`There was an error: ${response.error-type}`);
+  }
+}
+
+async function callEmUp(city) {
+  const response = await CurrencyService.getExchange();
+  getElements(response);
+}
+
+$(document).ready(function() {
+  $('#button').click(function() {
+    let myVar = 
+    callEmUp();
+  });
+});
