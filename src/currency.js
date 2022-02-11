4017a2391035cc0074e99fe62f13475ca7aec61f
Ryan Ashby <rjapsu160@gmail.com>
2022-02-11 09:20:59 -0800

    add currency.js and write CurrencyService async/await method

---
+++ src/currency.js
@@ -0,0 +1,13 @@
+export default class CurrencyService {
+  static getExchange() {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+        if (!response.ok) {
+          throw Error(response.error-type);
+        }
+        return response.json(); 
+    } catch(error) {
+      return error.message;
+    }
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
+    new HtmlWebpackPlugin()
       title: 'currency',
       template: './src/index.html',
       inject: 'body'
